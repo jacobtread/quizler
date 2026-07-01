6344a7f894985654a081b41a016d02ac340688d0
jacobtread <jacobtread@gmail.com>
2026-07-02 08:24:00 +1200

    refactor: helper to replace the commands for timing constants

--- frontend/src/lib/constants.ts
+++ frontend/src/lib/constants.ts
@@ -1,3 +1,8 @@
+const ms = {
+  seconds: (value: number) => value * 1000,
+  minutes: (value: number) => ms.seconds(value * 60)
+};
+
 export const TOKEN_LENGTH: number = 5;
 
 export const MIN_PLAYER_NAME_LENGTH: number = 1;
@@ -15,15 +20,15 @@ export const MAX_QUESTIONS: number = 50;
 
 export const MAX_QUESTION_LENGTH = 400;
 
-export const MIN_ANSWER_TIME: number = 1000; /* 1s */
-export const MAX_ANSWER_TIME: number = 1000 * 60 * 30; /* 30mins */
+export const MIN_ANSWER_TIME: number = ms.seconds(1);
+export const MAX_ANSWER_TIME: number = ms.minutes(30);
 
 export const MIN_SCORE: number = 0;
 export const MAX_MAX_SCORE: number = 10_000;
 export const MAX_BONUS_SCORE: number = 10_000;
 
-export const MIN_BONUS_TIME = 1000; /* 1s */
-export const MAX_BONUS_TIME = 1000 * 60 * 30; /* 30mins */
+export const MIN_BONUS_TIME = ms.seconds(1);
+export const MAX_BONUS_TIME = ms.minutes(30);
 
 export const MIN_MAX_PLAYERS: number = 1; /* 1 max player min */
 export const MAX_MAX_PLAYERS: number = 50; /* 50 max player limit to games */
